8b8960573fdb7d1d19118543382b8a51709fbb58
Aryan Jain <88190525+aryanjaindigitie@users.noreply.github.com>
2022-01-03 21:22:26 +0530

    Add files via upload

---
+++ precache-manifest.f6608312dbf98b7ea15ace62b5b5ab4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5326b822c0b512430910",
+    "url": "/static/css/main.44f89902.chunk.css"
+  },
+  {
+    "revision": "5326b822c0b512430910",
+    "url": "/static/js/main.5326b822.chunk.js"
+  },
+  {
+    "revision": "98ea6ddaa4256b937adc",
+    "url": "/static/js/1.98ea6dda.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f52cd362ea4697eb9ea6b3e5c966ff5",
+    "url": "/index.html"
+  }
+];
